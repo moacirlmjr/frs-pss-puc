bfa73eacf22b4a766913c55785b2e157fa778108
givanildesantos.tutora <givanildesantos.tutora@00d5c964-08ae-f14f-4943-c472347793ef>
2014-06-01 12:28:28 +0000

    Mudanças no Contexto

--- FRS-PSS/src/br/com/frs/agents/strategies/Contexto.java
+++ FRS-PSS/src/br/com/frs/agents/strategies/Contexto.java
@@ -1,16 +1,15 @@
 package br.com.frs.agents.strategies;
 
-
 public class Contexto {
-	
-	 private Estrategia estrategia;
-	 
-	    public Contexto(Estrategia estrategia) {
-	        this.estrategia = estrategia;
-	    }
-	 
-	    public void executeEstrategia() {
-	        estrategia.execute();
-	    }
+
+	private Estrategia estrategia;
+
+	public Contexto(Estrategia estrategia) {
+		this.estrategia = estrategia;
+	}
+
+	public void executeEstrategia() {
+		estrategia.execute();
+	}
 
 }
